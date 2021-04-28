bc94b7631c0af5ecaca20dbecf83aaad43246e8e
Christian Effiong <norexus007@gmail.com>
2021-04-28 14:05:00 +0100

    test

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -13,9 +13,9 @@ describe('AppController', () => {
   });
 
   describe('getHello', () => {
-    it('should return "Hello Universe!"', () => {
+    it('should return "Hello Galaxy!"', () => {
       const appController = app.get<AppController>(AppController);
-      expect(appController.getHello()).toBe('Hello Universe!');
+      expect(appController.getHello()).toBe('Hello Galaxy!');
     });
   });
 });
